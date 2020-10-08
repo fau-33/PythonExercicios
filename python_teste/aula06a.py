1fb4df83b3427915445b668e285fc9ffc6be0cae
Flávio Félix <fau_felix30@hotmail.com>
2020-10-08 19:47:01 -0300

    Feito a aula sobre tipo primitivos

---
+++ python_teste/aula06a.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite um valor: "))
+n2 = int(input("Digite outro valor: "))
+s = n1 + n2
+#print(f'A soma entre {n1} + {n2} = {s}')
+print('A soma entre {} + {} = {}'.format(n1, n2, s))
+
